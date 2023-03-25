d0b52e7bd73d83d012bbc4d5cd85e8ca288395e8
Sheroze Ali <sherozeali777@gmail.com>
2023-03-25 17:55:20 +0500

    feat: adjust folder structure and absolute paths added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Paragraph from "./components/ui/Paragraph";
-import LargeHeading from "./components/ui/Paragraph";
+import Paragraph from "../components/ui/Paragraph";
+import LargeHeading from "../components/ui/Paragraph";
 
 export default function Home() {
   return (
     <main className="">
       <Paragraph size="sm">SHEROZE ALI SHAH</Paragraph>
-      <LargeHeading size="sm">SHEROZE ALI SHAH</LargeHeading>
+      <LargeHeading size="sm">I am a Senior Software Engineer</LargeHeading>
     </main>
   );
 }
