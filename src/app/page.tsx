61ca21d4338e3b64f3554a1f81fae02e6d37cae6
Sheroze Ali <sherozeali777@gmail.com>
2023-03-25 17:44:22 +0500

    feat: added a LargeHeading component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Paragraph from "./components/ui/Paragraph";
+import LargeHeading from "./components/ui/Paragraph";
 
 export default function Home() {
   return (
     <main className="">
       <Paragraph size="sm">SHEROZE ALI SHAH</Paragraph>
+      <LargeHeading size="sm">SHEROZE ALI SHAH</LargeHeading>
     </main>
   );
 }
