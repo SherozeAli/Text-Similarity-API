9dca05e1cf04bca103e8c07da9404359954b4d3c
Sheroze Ali <sherozeali777@gmail.com>
2023-03-25 21:59:20 +0500

    feat: integrate dark/light theme, navbar component and body.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Toast, Toaster } from "@/components/ui/toast";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
@@ -12,16 +14,21 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("bg-white text-size-900 antialiased", inter.className)}
+      className={cn("bg-white text-slate-900 antialiased", inter.className)}
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
-        <Providers>{children}</Providers>
+        <Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+          <Toaster position="bottom-right" />
 
-        {/**
-         * Allow for more height on mobile devices
-         */}
+          {/* <MobileMenu /> */}
 
-        <div className="h-40 md:hidden"></div>
+          <main>{children}</main>
+        </Providers>
+
+        {/* Allow more height for mobile menu on mobile */}
+        <div className="h-40 md:hidden" />
       </body>
     </html>
   );
